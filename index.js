7fc7d06f83baf1a0309abeecd18c9c4a3ed06925
lee mwangi <lee.mwangi1@student.moringaschool.com>
2024-07-02 15:15:51 +0300

    thankyou

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+// Function to find the year the Denver Broncos won the Super Bowl
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
